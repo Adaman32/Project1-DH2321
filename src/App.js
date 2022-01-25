3116ae41464a644d8ce03480a57e98ec2b187149
Adam <adam.cerven@gmail.com>
2022-01-25 16:08:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -271,7 +271,7 @@ const App = () => {
             showIndividualData={setSelectedIndividual}
           />
         </g>
-        <g className="hobbies" transform={`translate(${innerWidth/2 + 550}, 500)`}>
+        <g className="hobbies" transform={`translate(${innerWidth/2 + 500}, 380)`}>
           <IndividualData 
             selectedIndividual={selectedIndividual}
             tickSpacing = {25}
